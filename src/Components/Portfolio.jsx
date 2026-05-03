73b2d635559144c62bd866aaac7e8677813c843a
YiLiang <2025210387@stu.hit.edu.cn>
2026-05-03 02:55:29 +0000

    add a new image and update the portfolio component to use the new image.

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "凶邪黑兽.jpg";
+import image from "重铸大刀.jpg";
 
-const imageAltText = "获取命格";
+const imageAltText = "把大刀变成克豹的形状";
 
 /**
  * Project list
